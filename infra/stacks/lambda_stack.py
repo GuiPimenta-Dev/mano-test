5610e00e2ebe72023ebcffb5e895bf75c80b189d
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-06-22 14:22:35 -0300

    initial

---
+++ infra/stacks/lambda_stack.py
@@ -0,0 +1,19 @@
+from functions.hello_world.config import HelloWorldConfig
+from docs.config import DocsConfig
+from aws_cdk import Stack
+from constructs import Construct
+from infra.services import Services
+
+
+class LambdaStack(Stack):
+    def __init__(self, scope: Construct, context, **kwargs) -> None:
+
+        super().__init__(scope, f"{context.name}-Lambda-Stack", **kwargs)
+
+        self.services = Services(self, context)
+
+        # Docs
+        DocsConfig(self.services)
+
+        # HelloWorld
+        HelloWorldConfig(self.services)
